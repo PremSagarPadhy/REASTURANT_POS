0819e8183e1431fb588e704da695738190cf616f
Prem Sagar Padhy <sagarprempadhy@gmail.com>
2025-06-29 13:21:25 +0530

    commit

--- pos-frontend/vite.config.js
+++ pos-frontend/vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     port: 3000,
